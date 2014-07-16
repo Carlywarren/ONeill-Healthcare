67b4f15bfb53607a6779d570fca1689e71ca384e
Milan <milanerak90@gmail.com>
2014-07-16 12:01:00 -0400

    adding blog page

---
+++ blog.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author"      content="Milan Erak (perceptionmm.com)">
+	
+	<title>O'Neill Healthcare & Managment</title>
+
+	<link rel="shortcut icon" href="assets/images/favicon.ico">
+	
+	<!-- FancyBox -->
+    <link rel="stylesheet" href="assets/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+    <!-- Optionally add helpers - button, thumbnail and/or media -->
+    <link rel="stylesheet" href="assets/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+    <link rel="stylesheet" href="assets/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen" >
+	<link rel="stylesheet" href="assets/css/main.css">
+
+
+
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+
+
+
+	<?php $page = "home"; ?>
+</head>
+
+<body class="home">
+
+<?php include 'inc/nav.php'; ?>
+
+	<!-- Header -->
+	<header id="head">
+		<div class="container">
+			<div class="row">
+				<div class="titles">
+					<h1 class="lead">Blog</h1>
+					<p class="tagline">Celebrating 50 years of providing quality care</p>
+					<p><a href="contact.php" class="btn btn-default btn-lg" role="button">Schedule a Tour</a> <a class="btn btn-action btn-lg fancybox-media" role="button" href="https://www.youtube.com/watch?v=vmFqp1uatmo" >Video Guide</a></p>
+				</div>
+				
+			</div>
+
+		</div>
+
+	</header>
+	<!-- /Header -->
+
+	<!-- Intro -->
+	<div class="container text-center">
+		<br> <br>
+		<h2 class="thin">The Future of Healthcare.</h2>
+		<p class="text-muted">
+			Close to Home. Close to Your Heart. 
+		</p>
+	</div>
+	<!-- /Intro-->
+
+	<!-- Highlights - jumbotron -->
+	<div class="jumbotron top-space">
+		<div class="container">
+			
+			<h3 class="text-center thin">Reasons to choose us</h3>
+			
+			<div class="row">
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption">
+						<h4 class="stars">
+							<i class="fa fa-star fa-1g"></i>
+							<i class="fa fa-star fa-2x"></i>
+							<i class="fa fa-star fa-3x"></i>
+							<i class="fa fa-star fa-2x"></i>
+							<i class="fa fa-star fa-1g"></i>
+						</h4> 
+						<h4>Quality </h4></div>
+					<div class="h-body text-center">
+						<p>Quality means doing it right when no one is looking.</p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fa fa-users fa-3x"></i>Support</h4></div>
+					<div class="h-body text-center">
+						<p>The greatest joys in life are found not only in what we do and feel, but also in our quiet hopes and labors for others. </p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fa fa-heart fa-3x"></i>Care</h4></div>
+					<div class="h-body text-center">
+						<p>If everyone is moving forward together, then success takes care of itself.</p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fa fa-smile-o fa-3x"></i>Happiness</h4></div>
+					<div class="h-body text-center">
+						<p>It is not how much we have, but how much we enjoy, that makes happiness.</p>
+					</div>
+				</div>
+			</div> <!-- /row  -->
+		
+		</div>
+	</div>
+	<!-- /Highlights -->
+
+	<!-- container -->
+	<div class="container">
+
+		<h2 class="text-center top-space">Frequently Asked Questions</h2>
+		<br>
+
+		<div class="row">
+			<div class="col-sm-6">
+				<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit?</h3>
+				<p>Integer elementum mattis faucibus. Aliquam at laoreet nisl. Aliquam eleifend nunc dolor, in ullamcorper urna gravida eu.</p>
+			</div>
+			<div class="col-sm-6">
+				<h3> Aenean eu ligula in diam facilisis adipiscing sed quis purus?</h3>
+				<p>
+					Donec ultrices consectetur elit, ut dapibus tortor. Praesent rutrum, neque in convallis mollis, tortor urna hendrerit massa, in semper justo dolor a nibh.</p>
+			</div>
+		</div> <!-- /row -->
+
+		<div class="row">
+			<div class="col-sm-6">
+				<h3>Vestibulum mattis tempor mi eu volutpat. Quisque hendrerit eleifend rhoncus?</h3>
+				<p>
+					Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+
+			</div>
+			<div class="col-sm-6">
+				<h3>Quisque commodo tellus id varius varius?</h3>
+				<p>Fusce eget luctus mauris. Duis eu venenatis tortor. Maecenas ac scelerisque ligula. Pellentesque dapibus est nec elit egestas faucibus nec sed urna.</p>
+			</div>
+		</div> <!-- /row -->
+
+		<div class="jumbotron top-space">
+			<h3>We invite you to visit us, talk things over, and decide for yourself.</h3>
+			<h4>Learn more about all of our services.</h4>
+     		<p class="text-right"><a href="services.php" class="btn btn-success btn-large">Learn more »</a></p>
+  		</div>
+
+	</div>	<!-- /container -->
+
+	<footer id="footer" class="top-space">
+
+		<?php include 'inc/footer.php'; ?>
+
+	</footer>	
+
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="assets/js/headroom.min.js"></script>
+	<script src="assets/js/jQuery.headroom.min.js"></script>
+	<script src="assets/js/template.js"></script>
+	<script type="text/javascript" src="assets/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+    <script type="text/javascript" src="assets/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+    <script type="text/javascript" src="assets/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+    <script type="text/javascript" src="assets/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.6"></script>
+    <script type="text/javascript" src="assets/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+    <script>
+        //Start FancyBox
+                jQuery('.fancybox-media').fancybox({
+                    openEffect  : 'none',
+                    closeEffect : 'none',
+                    helpers : {
+                        media : {}
+                    }
+                });
+        </script>
+
+	
+
+</body>
+</html>
