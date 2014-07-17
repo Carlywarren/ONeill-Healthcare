b5920f53ad1a70fe739cd4d077e54e811ef37534
Milan <milanerak90@gmail.com>
2014-07-17 16:41:35 -0400

    lakewood page

---
+++ lakewood.php
@@ -0,0 +1,417 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author"      content="Milan Erak (perceptionmm.com)">
+	
+	<title>Lakewood - O'Neill Healthcare & Managment</title>
+
+	<link rel="shortcut icon" href="assets/images/favicon.ico">
+	
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen" >
+	<link rel="stylesheet" href="assets/css/main.css">
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+
+	<?php $page = "bb"; ?>
+
+</head>
+
+<body onload="initialize()">
+
+	<div id="fb-root"></div>
+	<script>
+		(function(d, s, id) {
+	  		var js, fjs = d.getElementsByTagName(s)[0];
+	  		if (d.getElementById(id)) return;
+	  		js = d.createElement(s); js.id = id;
+	  		js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=557918634301713&version=v2.0";
+	  		fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+
+		
+	</script>
+
+
+	<!-- Fixed navbar -->
+	<?php include 'inc/nav.php'; ?>
+	<!-- /.navbar -->
+
+
+	<header id="head" class="lakewood"></header>
+
+	<!-- container -->
+	<div class="container">
+		
+		<ol class="breadcrumb">
+			<li><a href="index.php">Home</a></li>
+			<li class="active">Lakewood</li>
+		</ol>
+
+		<div class="row">
+			
+			<!-- Sidebar -->
+			<aside class="col-md-4 sidebar sidebar-left">
+
+
+
+				<div class="row widget">
+					<div class="col-xs-12">
+						<p><img src="assets/images/logos/LAKElogo.png" alt="Center Ridge North Ridgeville Logo" class="img-responsive full-img"></p>
+						
+					</div>
+				</div>
+
+				<div class="row widget">
+					<div class="col-xs-12">
+						<ul class="mini-nav">
+							<div class="row">
+							<li class="call col-md-12">
+								<a href="tel:+4408713474">
+									<button class="btn-block">
+										<div class="phonehide">
+											<i class="fa fa-phone fa-2"></i>    Call 
+										</div>
+										<div class="phone-num">440-871-3474</div>
+									</button> 
+								</a>
+								
+							</li>
+
+							<li class="visit col-md-12" >
+								<a href="#map_canvas">
+								<button class="btn-block" >
+										<i class="fa fa-location-arrow fa-2"></i>    Visit
+								</button>
+								</a>
+							</li>
+
+							<li class="write col-md-12">
+								<button class="write btn-block">
+									<i class="fa fa-pencil fa-2"></i>     Write
+								</button>
+							</li>
+						</div>
+						</ul>
+
+						<div class="bbform form">
+
+		                  	<div class="form-group">
+
+			                  	<label for="exampleInputEmail1">Name</label>
+			                    <input type="text" class="form-control" name="fname" id="fname" placeholder="First & Last Name" />
+			                   	<label for="exampleInputEmail1">Email</label>
+			                    <input type="text" class="form-control" name="lname" id="lname" placeholder="Email" />
+			                    <label for="exampleInputEmail1">Message</label>
+			                    <textarea name="message" rows="7" class="form-control" id="message" placeholder="Comments / Questions"></textarea>
+
+		                	</div>
+
+		                   	<button style type="submit" class="btn btn-default btn-block">Submit</button>
+
+						</div>
+						
+						
+					</div>
+				</div>
+				<div class="row">
+				<div class="widget col-sm-6 col-md-12 col-lg-12">
+
+					<img src="assets/images/rooms/lk1.jpg" class="img-responsive full-img"/>
+				</div>
+				
+				<div class="widget col-sm-6 col-md-12 col-lg-12">
+
+					<img src="assets/images/rooms/lk2.jpg" class="img-responsive full-img"/>
+					
+
+				</div>
+				<div class="widget col-sm-6 col-md-12 col-lg-12">
+
+					<img src="assets/images/rooms/lk3.jpg" class="img-responsive full-img"/>
+					
+
+				</div>
+
+				<div class="widget col-sm-6 col-md-12 col-lg-12">
+
+					<img src="assets/images/rooms/floorholder.jpg" class="img-responsive full-img"/>
+					
+
+				</div>
+				</div>
+				
+				
+
+			</aside>
+			<!-- /Sidebar -->
+
+			<!-- Article main content -->
+			<article class="col-md-8 maincontent">
+				<header class="page-header">
+					<h1 class="page-title">Welcome to Center Ridge Health Campus!</h1>
+				</header>
+				<p>Center Ridge Health Campus is a 150 bed, long-term care and rehabilitation center in North Ridgeville, Ohio. Center Ridge Health Campus believes in the dignity of human life at all stages.  Center Ridge Health Campus is also a premier provider of rehabilitation services in Lorain County, and since 1986 has been offering quality inpatient and outpatient therapy services to the community.</p>
+
+				<h2>Our Story</h2>
+				<p>
+					In 1986, the second generation of the O’Neill family, headed by John T. O’Neill, LNHA, started Center Ridge Health Campus, a 150 bed skilled nursing facility. The O’Neill family has built a solid reputation in the west side community for providing excellent, comprehensive care since 1962, with the opening of Bradley Bay Health Center in Bay Village.
+				</p>
+				<p>
+					Center Ridge health Campus is recognized as a premier health care facility. This success is a direct result of the commitment to maintaining a sincere, respectful concern for each resident and to providing the finest health care services in Northeastern Ohio.
+				</p>
+
+				<p>Our mission is to provide the highest quality of life to each resident, utilizing a holistic approach that involves family and community, while encouraging active participation in a progressive rehabilitation plan. All of this takes place in a caring, supportive, and homelike atmosphere.</p>
+				
+
+				<h2>Accommodations</h2>
+				<p>Located in North Ridgeville on beautifully maintained acreage, Center Ridge Health Campus is just minutes from Elyria. Outdoor porches, landscaped walkways, and courtyards provide ample opportunities to enjoy the attractive grounds, which surround the home.</p>
+				
+
+				<blockquote>All of the resident accommodations in this safety designed, ranch style nursing center are spacious, air-conditioned and cheerful. Tastefully appointed dining rooms and numerous lounges are welcome places for residents to share meals, enjoy events and parties, or just visit.</blockquote>
+
+				
+				
+				<h3>Dining</h3>
+				
+				<p>Dining at Center Ridge Health Campus is like being able to enjoy your favorite restaurant at every meal. Menus are individually tailored and prepared around residents' needs and interests. Soups made from scratch, old fashioned hamburgers, homemade pumpkin rolls and zucchini bread are just some of the residents' favorites to choose from. And because every dish is personally reviewed by our Dietitians, you can be sure each meal exceeds all acceptable nutritional standards.</p>
+				<p>Although residents are encouraged to enjoy their meals in one of our tastefully decorated dining rooms, they also have the option of eating in their rooms or enjoying "breakfast in bed." But you're not done having your say. Center Ridge also has a Food Committee where residents are encouraged to participate and give ongoing feedback on menu satisfaction, as well as recommendations for additional dishes.</p>
+
+
+
+				<h3>Activities</h3>
+				<div class="row">
+			
+				
+
+				<div class="col-md-6">
+					
+					<p>
+						At Center Ridge Health Campus we work to promote a healthy and active lifestyle for each and every resident. We provide and support a variety of recreational programs, tailored to your needs and interests. You'll have the opportunity to participate in a number of activities you're familiar with, as well as new experiences that are sure to become favorites. No matter what you choose to do, participation will promote physical, cognitive and emotional well-being, as well as empower you to be more active, self-directing and spontaneous. But best of all, you'll have fun and foster new friendships.
+					</p>
+
+					<p>A Sample of Events from our Activity Calendar</p>
+					<ul>
+						<li>Cards and Trivia Games</li>
+						<li>Bus Trips to Local Restaurants</li>
+						<li>Prayer Services</li>
+						<li>Garden, Baking &amp; Book Clubs</li>
+						<li>Visiting Musical Groups</li>
+						<li>Sing-alongs</li>
+					</ul>
+				</div>
+				<div class="col-md-6">
+					<img src="assets/images/lakewood_pic.jpg" class="img-responsive full-img"/>
+				</div>
+				</div>
+
+				<h4>Thinking of something else you'd enjoy?</h4>
+				<p>Organize your own activity! Join the resident activity committee and suggest your own events or clubs. From a night at the horse races, to trips to the local fair, to Coffee and Fishing Clubs... we can always count on our residents to have the best ideas.</p>
+
+				<div class="fb-comments" data-href="http://perceptionmm.com/oldsite/clients/on/bradleybay.php" data-width="100%" data-numposts="5" data-colorscheme="light"></div>
+			</article>
+			<!-- /Article -->
+
+		</div>
+	</div>	<!-- /container -->
+	
+	<div id="map_canvas"></div>
+    <div id="directionsPanel"></div>
+
+	<footer id="footer">
+
+		<?php include 'inc/footer.php'; ?>
+	</footer>	
+		
+
+
+
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="assets/js/headroom.min.js"></script>
+	<script src="assets/js/jQuery.headroom.min.js"></script>
+	<script src="assets/js/template.js"></script>
+	
+	<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript">
+		var geocoder;
+		var directionsService;
+		var directionsDisplay;
+
+		function initialize() {
+		    var latlng = new google.maps.LatLng(41.383823, -82.055809);
+		    var myOptions = {
+		        zoom: 13,
+		        center: latlng,
+		        mapTypeId: google.maps.MapTypeId.ROADMAP,
+		        styles: [
+				{
+					featureType: "landscape",
+					stylers: [
+					{ saturation: -100 },
+					{ lightness: 65 },
+					{ visibility: "on" }
+					]
+				},{
+					featureType: "poi",
+					stylers: [
+					{ saturation: -100 },
+					{ lightness: 51 },
+					{ visibility: "simplified" }
+					]
+				},{
+					featureType: "road.highway",
+					stylers: [
+					{ saturation: -100 },
+					{ visibility: "simplified" }
+					]
+				},{
+					featureType: "road.arterial",
+					stylers: [
+					{ saturation: -100 },
+					{ lightness: 30 },
+					{ visibility: "on" }
+					]
+				},{
+					featureType: "road.local",
+					stylers: [
+					{ saturation: -100 },
+					{ lightness: 40 },
+					{ visibility: "on" }
+					]
+				},{
+					featureType: "transit",
+					stylers: [
+					{ saturation: -100 },
+					{ visibility: "simplified" }
+					]
+				},{
+					featureType: "administrative.province",
+					stylers: [
+					{ visibility: "off" }
+					]
+			/** /
+				},{
+					featureType: "administrative.locality",
+					stylers: [
+						{ visibility: "off" }
+					]
+				},{
+					featureType: "administrative.neighborhood",
+					stylers: [
+						{ visibility: "on" }
+					]
+					/**/
+				},{
+					featureType: "water",
+					elementType: "labels",
+					stylers: [
+					{ visibility: "on" },
+					{ lightness: -25 },
+					{ saturation: -100 }
+					]
+				},{
+					featureType: "water",
+					elementType: "geometry",
+					stylers: [
+					{ hue: "#ffff00" },
+					{ lightness: -25 },
+					{ saturation: -97 }
+					]
+				}
+				],
+		    };
+
+		    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+		    var marker = new google.maps.Marker({
+		        position: new google.maps.LatLng(41.383823, -82.055809),
+		        map: map,
+		        title: 'Center Ridge',
+		        clickable: true
+		    });
+
+		    var infowindow = new google.maps.InfoWindow({
+		        content: "Your address: <input id='clientAddress' type='text'>"+
+		                "<input type='button' onClick=getDir() value='Go!'>"
+		    });
+
+		    google.maps.event.addListener(marker, 'click', function () {
+		        infowindow.open(map, marker);
+		    });
+		    geocoder = new google.maps.Geocoder();
+		    directionsService = new google.maps.DirectionsService();
+		    directionsDisplay = new google.maps.DirectionsRenderer({
+		        suppressMarkers: false
+		    });
+		    directionsDisplay.setMap(map);
+		    
+		    directionsDisplay.setPanel(document.getElementById("directionsPanel"));
+		   	
+		}
+
+		function getDir() {
+		    geocoder.geocode({
+		        'address': document.getElementById('clientAddress').value
+		    },
+
+		    function (results, status) {
+		        if (status == google.maps.GeocoderStatus.OK) {
+		            var origin = results[0].geometry.location;
+		            var destination = new google.maps.LatLng(41.383823, -82.055809);
+
+		            var request = {
+		                origin: origin,
+		                destination: destination,
+		                travelMode: google.maps.DirectionsTravelMode.DRIVING
+		            };
+
+		            directionsService.route(request, function (response, status) {
+		                if (status == google.maps.DirectionsStatus.OK) {
+		                    directionsDisplay.setDirections(response);
+		                }
+		            });
+
+		        } else {
+		            document.getElementById('clientAddress').value =
+		                "Directions cannot be computed at this time.";
+		        }
+		    });
+		}
+    </script>
+
+    <script>
+	
+		$(".phone-num").hide();
+		$(".call").click(function(){
+			$(".phone-num").show();
+			$(".phonehide").hide();
+		});
+
+		$(".bbform").hide();
+		$(".write").click(function(){
+			$(".mini-nav").css("border-bottom" , "none");
+			$(".bbform").slideDown(1000);
+
+		});
+	</script>
+
+	<!-- Google Maps 
+	<script src="https://maps.googleapis.com/maps/api/js?key=&amp;sensor=false&amp;extension=.js"></script> 
+	<script src="assets/js/google-map.js"></script>
+	-->
+
+</body>
+</html>
